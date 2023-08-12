0ae265f2a5fb417f398dd63937fb76454156bf70
tirouzh <ctirouzh@gmail.com>
2023-08-12 23:52:59 +0330

    Go1.21.0: test builtin min() and max() functions

---
+++ release1.21/minmax/minmax_test.go
@@ -0,0 +1,42 @@
+package minmax
+
+import (
+	"cmp"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRelease121_Min(t *testing.T) {
+	type testCase[T cmp.Ordered] struct {
+		name           string
+		inputs         []T
+		expectedMinMax [2]T
+	}
+
+	testCases := []testCase[int]{
+		{"min&max(1,2)", []int{1, 2}, [2]int{1, 2}},
+		{"min&max(-1,2)", []int{-1, 2}, [2]int{-1, 2}},
+		{"min&max(-10)", []int{-10}, [2]int{-10, -10}},
+		{"min&max(-10,-20,53)", []int{-10, -20, 53}, [2]int{-20, 53}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			switch len(tc.inputs) {
+			case 1:
+				assert.Equal(t, tc.expectedMinMax[0], min(tc.inputs[0]))
+				assert.Equal(t, tc.expectedMinMax[1], max(tc.inputs[0]))
+			case 2:
+				assert.Equal(t, tc.expectedMinMax[0], min(tc.inputs[0], tc.inputs[1]))
+				assert.Equal(t, tc.expectedMinMax[1], max(tc.inputs[0], tc.inputs[1]))
+			case 3:
+				assert.Equal(t, tc.expectedMinMax[0], min(tc.inputs[0], tc.inputs[1], tc.inputs[2]))
+				assert.Equal(t, tc.expectedMinMax[1], max(tc.inputs[0], tc.inputs[1], tc.inputs[2]))
+			default:
+				assert.True(t, true)
+			}
+
+		})
+	}
+
+}
